beacb0be08019d57787d5b7314963e96c8df2b95
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-03-16 08:28:00 +0530

    feat: register UserStreak model in admin; add UserStreakInfo admin configuration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import UserRegistrations
+from .models import UserRegistrations,UserStreak
 
 class UserInfo(admin.ModelAdmin):
     list_display = ('username', 'email','password')
 admin.site.register(UserRegistrations, UserInfo)
+
+class UserStreakInfo(admin.ModelAdmin):
+    list_display = ('user', 'streak_count','last_visit','longest_streak')
+admin.site.register(UserStreak, UserStreakInfo)
